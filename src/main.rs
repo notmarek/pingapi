5f06ff4cd1b792524dc38ac395f6ce9f6ccd675a
Packer <25778864+SirPacker@users.noreply.github.com>
2021-04-28 15:27:48 -0400

    Send user-agent with requests

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,7 @@ async fn ping_url(url: &String, timeout: u64) {
     let client = Client::builder()
         .timeout(Duration::new(timeout, 0))
         .connector(Connector::new().ssl(builder.build()).finish())
+        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0")
         .finish();
 
     let response = client.get(url).send().await;
