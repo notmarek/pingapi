8e1dd372f50a51ff2c5b20b7e8cdd20fc7ac99a1
Marek Veselý <veselym@spstrutnov.cz>
2021-08-30 21:45:13 +0200

    Use reqwest and make proxy optional

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 extern crate redis;
 
-use std::{env, thread};
-use std::collections::HashMap;
-use std::str::from_utf8;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
-
 use actix_cors::Cors;
-use actix_socks::SocksConnector;
-use actix_web::{App, Error, get, http, HttpResponse, HttpServer, post, Responder, web};
-use actix_web::client::{Client, Connector};
+use actix_web::{get, http, post, web, App, Error, HttpResponse, HttpServer, Responder};
 use log::{debug, info, trace};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use redis::Commands;
+use reqwest::Client;
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::str::from_utf8;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::{env, thread};
 
 #[derive(Deserialize)]
 struct Url {
@@ -24,33 +22,37 @@ struct Urls {
     urls: Vec<String>,
 }
 
-
 // ------------------------------------------------------------------------------
 // background tasks
 // ------------------------------------------------------------------------------
 
 // returns a new connection to redis server
 fn get_redis_con() -> redis::Connection {
-    let client = redis::Client::open("redis://127.0.0.1")
-        .expect("Cannot connect to local redis server");
-    client.get_connection()
+    let client =
+        redis::Client::open("redis://127.0.0.1").expect("Cannot connect to local redis server");
+    client
+        .get_connection()
         .expect("Connection to redis server failed")
 }
 
 // fetches the current status of given url from the redis server
 fn get_status(url: &String) -> HashMap<String, String> {
     let mut con = get_redis_con();
-    let ex: bool = con.exists(format!("ping:{}", url))
+    let ex: bool = con
+        .exists(format!("ping:{}", url))
         .expect("Failed to determine existence of url");
     if !ex {
         con.sadd::<&str, &String, bool>("urls", url)
             .expect("Failed to add url to redis urls list");
-        con.hset_multiple::<String, &str, &String, bool>(format!("ping:{}", url), &[
-            ("url", url),
-            ("time", &"0".to_string()),
-            ("status", &"unknown".to_string())
-        ])
-            .expect("Failed to create new redis entry for url");
+        con.hset_multiple::<String, &str, &String, bool>(
+            format!("ping:{}", url),
+            &[
+                ("url", url),
+                ("time", &"0".to_string()),
+                ("status", &"unknown".to_string()),
+            ],
+        )
+        .expect("Failed to create new redis entry for url");
     }
 
     con.hgetall::<String, HashMap<String, String>>(format!("ping:{}", url))
@@ -61,12 +63,15 @@ fn get_status(url: &String) -> HashMap<String, String> {
 fn update_status(url: &String, status: &str) {
     let mut con = get_redis_con();
     let time = get_epoch();
-    con.hset_multiple::<String, &str, &String, bool>(format!("ping:{}", url), &[
-        ("url", url),
-        ("time", &time.as_secs().to_string()),
-        ("status", &status.to_string())
-    ])
-        .expect("Failed to update redis entry for url");
+    con.hset_multiple::<String, &str, &String, bool>(
+        format!("ping:{}", url),
+        &[
+            ("url", url),
+            ("time", &time.as_secs().to_string()),
+            ("status", &status.to_string()),
+        ],
+    )
+    .expect("Failed to update redis entry for url");
 }
 
 // returns the current UNIX_EPOCH as Duration
@@ -77,78 +82,45 @@ fn get_epoch() -> Duration {
 }
 
 // pings given url and upon finish calls update_status with the result
-async fn ping_url(url: &String, timeout: u64, socks_ip: &'static str) {
+async fn ping_url(url: &String, timeout: u64) {
     trace!("Pinging {} with timeout {}", url, timeout.to_string());
+    let socks_ip = env::var("SOCKS_IP").unwrap_or(String::new());
+    let socks_user = env::var("SOCKS_USER").unwrap_or(String::new());
+    let socks_pass = env::var("SOCKS_PASS").unwrap_or(String::new());
+    let mut builder = Client::builder();
 
-    // required for handling https requests
-    let mut builder = SslConnector::builder(SslMethod::tls())
-        .unwrap();
+    if !socks_user.is_empty() && !socks_pass.is_empty() && !socks_ip.is_empty() {
+        let proxy = reqwest::Proxy::all(&format!("{}", socks_ip)).expect("proxy should exist");
+        builder = builder.proxy(proxy.basic_auth(&socks_user, &socks_pass));
+    }
 
-    // disable cert verification, due to lack of local cert
-    builder.set_verify(SslVerifyMode::NONE);
+    let client = builder.user_agent("Mozilla/5.0 (piracy.moe; Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0").build().unwrap();
 
-    let socks_user = env::var("SOCKS_USER").expect("env SOCKS_USER not found");
-    let socks_pass = env::var("SOCKS_PASS").expect("env SOCKS_PASS not found");
-
-    let client = Client::builder()
-        .timeout(Duration::new(timeout, 0))
-        .connector(Connector::new().ssl(builder.build()).finish())
-        .connector(
-            Connector::new().connector(
-                SocksConnector(
-                    socks_ip,
-                    socks_user.to_string(),
-                    socks_pass.to_string())
-                )
-            .finish()
-        )
-        .header("DNT", "1")
-        .header("Referer", "https://piracy.moe/")
-        .header("Pragma", "no-cache")
-        .header("Cache-Control", "no-cache")
-        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-        .header("Accept-Language", "de,en-US;q=0.7,en;q=0.3")
-        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
-
-
-    let response = client.finish().get(url).send().await;
+    let response = client.get(url).send().await;
 
     match response {
-        Ok(mut res) => {
+        Ok(res) => {
             debug!("Ping request of {} succeeded with {:?}", url, res);
-            let status = &res.status().as_u16();
-            let safe: &[u16] = &[200, 300, 301, 302, 307, 308];
+            let status = res.status();
+            let headers = res.headers();
 
-            if safe.contains(status) {
+            if status.is_success() || status.is_redirection() {
                 return update_status(url, "up");
             }
-
-            let body = res.body().await;
-            match body {
-                Ok(b) => {
-                    debug!("{} has body {:?}", url, std::str::from_utf8(b.as_ref()).unwrap());
-                }
-                Err(e) => {
-                    debug!("{} failed to parse body {:?}", url, e);
-                }
-            }
-
-            // separate block for immutable borrow of response
-            {
-                let headers = res.headers();
-                debug!("{} has headers {:?}", url, headers);
-                if headers.contains_key("Server") {
-                    let server = headers.get("Server")
-                        .expect("Failed to parse Server response header")
-                        .to_str().unwrap();
-                    debug!("Server of {} is {}", url, server);
-                    let unknown: &[u16] = &[401, 403, 503, 520];
-                    let forbidden: &u16 = &403;
-                    if unknown.contains(status) && server.eq("cloudflare") ||
-                        status.eq(forbidden) && server.eq("ddos-guard") {
-                        info!("Unknown HTTP status of {}: {}", url, status);
-                        return update_status(url, "unknown");
-                    }
+            debug!("{} has headers {:?}", url, headers);
+            if headers.contains_key("Server") {
+                let server = headers
+                    .get("Server")
+                    .expect("Failed to parse Server response header")
+                    .to_str()
+                    .unwrap();
+                debug!("Server of {} is {}", url, server);
+                if (status.is_client_error() || status.is_server_error())
+                    && server.eq("cloudflare")
+                    && server.eq("ddos-guard")
+                {
+                    info!("Unknown HTTP status of {}: {}", url, status);
+                    return update_status(url, "unknown");
                 }
             }
 
@@ -166,13 +138,15 @@ async fn ping_url(url: &String, timeout: u64, socks_ip: &'static str) {
 }
 
 // background task, which checks when to update known entries of the redis server
-async fn background_scan(interval: u64, timeout: u64, socks_ip: &'static str) {
+async fn background_scan(interval: u64, timeout: u64) {
     debug!("Running background task");
     let mut con = get_redis_con();
-    let mut urls = con.smembers::<&str, Vec<String>>("urls")
+    let mut urls = con
+        .smembers::<&str, Vec<String>>("urls")
         .expect("Failed to retrieve urls from redis urls list");
     urls.retain(|url| {
-        let t = con.hget::<String, &str, String>(format!("ping:{}", url), "time")
+        let t = con
+            .hget::<String, &str, String>(format!("ping:{}", url), "time")
             .expect("Failed to retrieve urls from redis urls list")
             .parse::<u64>()
             .expect("Failed to convert string to u64");
@@ -180,13 +154,12 @@ async fn background_scan(interval: u64, timeout: u64, socks_ip: &'static str) {
     });
     info!("Found {} urls to be pinged", urls.len());
     if urls.len() > 0 {
-        let p = urls.iter().map(|url| ping_url(url, timeout, socks_ip));
+        let p = urls.iter().map(|url| ping_url(url, timeout));
         for f in p {
             f.await;
         }
     }
 }
-
 
 // ------------------------------------------------------------------------------
 // web service
@@ -196,15 +169,13 @@ async fn background_scan(interval: u64, timeout: u64, socks_ip: &'static str) {
 async fn index() -> impl Responder {
     let url = env::var("CORS").expect("env CORS not found");
     debug!("Redirect to {}", url);
-    HttpResponse::Found()
-        .header("Location", url)
-        .finish()
+    HttpResponse::Found().header("Location", url).finish()
 }
 
 #[get("/health")]
 async fn health() -> impl Responder {
-    debug!("alive");
-    HttpResponse::Ok().header("Content-Type", "text/html; charset=UTF-8").body("OK")
+    debug!("Alive");
+    HttpResponse::Ok().body("OK")
 }
 
 #[post("/ping")]
@@ -233,8 +204,7 @@ async fn main() {
         let cors = Cors::default()
             .allowed_origin("http://localhost:8080")
             .allowed_origin_fn(|origin, _| {
-                let u = env::var("CORS")
-                    .expect("env CORS not found");
+                let u = env::var("CORS").expect("env CORS not found");
                 let cors_regex = regex::Regex::new(&*u).unwrap();
                 match from_utf8(origin.as_bytes()) {
                     Ok(origin_utf8) => cors_regex.is_match(origin_utf8),
@@ -254,12 +224,9 @@ async fn main() {
             .service(ping)
             .service(pings)
     })
-        .bind("0.0.0.0:5000")
-        .expect("Failed to launch web service")
-        .run();
-
-    let _socks_ip = env::var("SOCKS_IP").expect("env SOCKS_IP not found");
-    let socks_ip: &'static str = Box::leak(_socks_ip.into_boxed_str());
+    .bind("0.0.0.0:5000")
+    .expect("Failed to launch web service")
+    .run();
 
     info!("Starting pingapi");
     let interval = env::var("INTERVAL")
@@ -271,7 +238,7 @@ async fn main() {
         .parse::<u64>()
         .expect("Failed to convert TIMEOUT to u64");
     loop {
-        background_scan(interval, timeout, socks_ip).await;
+        background_scan(interval, timeout).await;
         thread::sleep(Duration::from_secs(timeout * 2));
     }
 }
